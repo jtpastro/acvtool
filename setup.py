9392419b8a73cf6f450cef6cbbe079d819cbdd48
Jonata Pastro <jtpastro@hotmail.com>
2022-11-11 14:45:37 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'javaobj==0.1.0',
-        'chameleon==3.1',
-        'pyyaml==3.12',
-        'lxml==4.1.1',
-        'six==1.11.0'],
+        'javaobj>=0.1.0',
+        'chameleon>=3.1',
+        'pyyaml>=3.12',
+        'lxml>=4.1.1',
+        'six>=1.11.0'],
     entry_points={
         'console_scripts': [
             'acv=acvtool:main',
